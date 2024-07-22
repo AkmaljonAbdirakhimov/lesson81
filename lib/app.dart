e4ad7d13196024bd421226efc09e3dc4f6d9ba68
Akmaljon Abdirakhimov <AkmaljonAbdirakhimov@gmail.com>
2024-07-22 14:30:41 +0500

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:lesson81/ui/screens/register_screen.dart';
+import 'package:lesson81/ui/screens/users_screen.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: UsersScreen(),
+    );
+  }
+}
